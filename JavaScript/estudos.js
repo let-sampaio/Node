1487bf97bd8e0410455d86b7acedf0fdc922a69b
LETICIA SAMPAIO DE SOUZA <90839815+let-sampaio@users.noreply.github.com>
2024-03-19 21:04:54 -0300

    Delete JavaScript directory

--- JavaScript/estudos.js
+++
@@ -1,9 +0,0 @@
-function myFunction(xml){
-    const xmlDoc = xml.responseXML;
-    const x = xmlDoc.getElementsByTagName("CD");
-    let table ="<tr><th>Artist</th><th>Title</th></tr>";
-    for(let i =0;i< x.length; i++){
-        table += "<tr><td>" + x[i].getElementsByTagName("ARTIST")
-        [0].childNodes[0].nodeValue + "</td></tr>";
-    }
-}document.getElementById("demo").innerHTML = tables;
